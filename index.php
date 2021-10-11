4f6e37bd1cdb51efb41520218028111a76734d6d
Sherica Ocbania <34484211+iennetastic@users.noreply.github.com>
2021-10-11 20:55:51 +1030

    Styling

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Practical 3: Current tasks</title>
+    <title>Content Management System</title>
     <meta charset="UTF-8" />
     <meta name="author" content="Sen Lin" />
     <script src="scripts/script.js" defer></script>
     <link rel="stylesheet" href="styles/style.css"/>
+    <link rel="shortcut icon" type="image/png" href="favicon-32x32.png"/>
+
+
 </head>
 <body>
-    <div class=wrapper>
-        <div class = container>
-        </div>
-        <div class=container id="tasklist">
-            <h1> Welcome to content management system</h1>
-            <p> Please enter your username and password</p>
+    <div class="wrapper">
+        <div class="container" id="tasklist">
+            <div class="logo"><img src="img/logo.png"></div>
 
-        
+            <h1>Login</h1> 
+            <div class="form">
             <form action="log-in.php" method="POST">
               <input name="username" type="text" placeholder="Enter the username" required/>
-              <input name="password" type="text" placeholder="Enter the password" required/>
+              <input name="password" type="password" placeholder="Enter the password" required/>
               <input type="submit" value="Log in"/>
           </form>
+</div>
 
         </div>
     </div>
