2c7a29909b447b74a61928d8a95a96766b3599a1
Kyuhyun-kim427 <69184413+Kyuhyun-kim427@users.noreply.github.com>
2021-10-15 19:02:20 +1030

    Update product-menu.inc.php

--- inc/product-menu.inc.php
+++ inc/product-menu.inc.php
@@ -1,5 +1,5 @@
 <ul id="menu">
-    <li><a class="no-underline" href="product-view.php">Product list</a></li>
+    <li><a class="no-underline" href="manage-product.php">Product list</a></li>
     <li><a class="no-underline" href="product-add.php">Add product</a></li>
     <li><a class="no-underline" href="product-history.php">Management Log</a></li>
 </ul>
