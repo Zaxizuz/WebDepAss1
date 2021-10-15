309c67db03540bc346c9b7d573d1938432fe05c1
Kyuhyun-kim427 <69184413+Kyuhyun-kim427@users.noreply.github.com>
2021-10-15 18:01:52 +1030

    Update product-delete-process.php

--- manager/product-delete-process.php
+++ manager/product-delete-process.php
@@ -2,16 +2,16 @@
     if (isset($_GET["id"])){
         require_once "inc/dbconn.inc.php";
         $sql = "UPDATE Product SET completed=1, tag='deleted', updated=now() WHERE id=?;";
-        $statement = mysqli_stmt_init($conn);
+        $statement = mysqli_stmt_init($link);
        mysqli_stmt_prepare($statement, $sql);
 
 
         mysqli_stmt_bind_param($statement, 'i', htmlspecialchars($_GET["id"]));
         if(mysqli_stmt_execute($statement)){
-            header("location: product-view.php");
+            header("location: manage-product.php");
         }else{
-            mysqli_error($conn);
+            mysqli_error($link);
         };
-        mysqli_close($conn);
+        mysqli_close($link);
     }
 ?>
