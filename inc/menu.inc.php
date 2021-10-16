f76cdabd516f1db02b0f49513e3a04108c10314b
Sherica Ocbania <34484211+iennetastic@users.noreply.github.com>
2021-10-16 11:48:32 +1030

    Menu fix

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -4,7 +4,7 @@
  
 <div id="menu">    
 <a href="index.php">Dashboard</a> 
-<a class="active" href="index.php">Dashboard</a> 
+<a href="index.php">Dashboard</a> 
 <a href="register.php">Add A User</a>
 <a href="manage-product.php">Manage Products</a>
 <a href="manage-users.php">Manage Users</a>
