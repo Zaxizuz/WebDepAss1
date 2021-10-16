b4dc8185682ce9cc8eaa4598a0239533121e71e9
Sherica Ocbania <34484211+iennetastic@users.noreply.github.com>
2021-10-16 11:47:55 +1030

    Menu Fix

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -1,9 +1,12 @@
 <div class="logo-sidebar">
     <img src="img/logo-dark.png" style="width: 100px">
 </div>
-
-<a class="active" href="index.php">Dashboard</a>
+ 
+<div id="menu">    
+<a href="index.php">Dashboard</a> 
+<a class="active" href="index.php">Dashboard</a> 
 <a href="register.php">Add A User</a>
 <a href="manage-product.php">Manage Products</a>
 <a href="manage-users.php">Manage Users</a>
-<a href="logout.php">Logout</a> 
+<a href="logout.php">Logout</a>  
+</div>
