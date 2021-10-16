49ae4351ff205e9984eff2a4dfeca4979cb12a1a
Sherica Ocbania <34484211+iennetastic@users.noreply.github.com>
2021-10-16 11:49:00 +1030

    Dashboard

--- inc/menu.inc.php
+++ inc/menu.inc.php
@@ -3,8 +3,7 @@
 </div>
  
 <div id="menu">    
-<a href="index.php">Dashboard</a> 
-<a href="index.php">Dashboard</a> 
+<a href="index.php">Dashboard</a>  
 <a href="register.php">Add A User</a>
 <a href="manage-product.php">Manage Products</a>
 <a href="manage-users.php">Manage Users</a>
